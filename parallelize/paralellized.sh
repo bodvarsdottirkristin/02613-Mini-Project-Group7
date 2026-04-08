e2ebf3cd5c5c8d3d3f7d79e939fe2c394e309ccf
bodvarsdottirkristin <s253093@dtu.dk>
2026-04-08 16:10:17 +0200

    agg log folder to paralellize

--- parallelize/paralellized.sh
+++ parallelize/paralellized.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#BSUB -J parallelized
+#BSUB -J logs/parallelized
 #BSUB -q hpc
 #BSUB -W 02:00
 #BSUB -n 7
 #BSUB -R "rusage[mem=512MB] span[hosts=1] select[model==XeonGold6126]"
-#BSUB -o parallelized.%J.out
-#BSUB -e parallelized.%J.err
+#BSUB -o logs/parallelized.%J.out
+#BSUB -e logs/parallelized.%J.err
 #BSUB -B 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
