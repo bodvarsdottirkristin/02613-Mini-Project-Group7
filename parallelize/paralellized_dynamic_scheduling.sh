c1cb4f4a5f0058bc0aa723abd338e9268dc351f7
bodvarsdottirkristin <s253093@dtu.dk>
2026-04-09 12:59:49 +0200

    add dynamic scheduling, missing in pr

---
+++ parallelize/paralellized_dynamic_scheduling.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -J logs/parallelized
+#BSUB -q hpc
+#BSUB -W 02:00
+#BSUB -n 7
+#BSUB -R "rusage[mem=512MB] span[hosts=1] select[model==XeonGold6126]"
+#BSUB -o logs/parallelized.%J.out
+#BSUB -e logs/parallelized.%J.err
+#BSUB -B 
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python paralellized_dynamic_scheduling.py 100
