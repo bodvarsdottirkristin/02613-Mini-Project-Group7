2e31650641a26d65ed283923578ab17d2fb91c12
bodvarsdottirkristin <s253093@dtu.dk>
2026-04-29 16:47:00 +0200

    adding logs and results for part 5 and 6 using the 2026 env

--- parallelize/paralellized_dynamic_scheduling.sh
+++ parallelize/paralellized_dynamic_scheduling.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#BSUB -J logs/parallelized
+#BSUB -J logs/parallelized_2026_dynamic
 #BSUB -q hpc
 #BSUB -W 02:00
 #BSUB -n 7
 #BSUB -R "rusage[mem=512MB] span[hosts=1] select[model==XeonGold6126]"
-#BSUB -o logs/parallelized.%J.out
-#BSUB -e logs/parallelized.%J.err
+#BSUB -o logs/parallelized_2026_dynamic.%J.out
+#BSUB -e logs/parallelized_2026_dynamic.%J.err
 #BSUB -B 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
-conda activate 02613
+conda activate 02613_2026
 
 python paralellized_dynamic_scheduling.py 100
